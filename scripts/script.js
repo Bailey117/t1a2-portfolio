a5dcd9ae083b911bf7281f89598ad8dc6270bf63
Bailey117 <bailey.evans0202@gmail.com>
2023-01-24 20:50:12 +1100

    big update

--- scripts/script.js
+++ scripts/script.js
@@ -4,16 +4,36 @@ var emtshape = document.getElementById("skillcardshape")
 function scrollAnim() {
     var ypos = window.pageYOffset;
 
-    if (ypos > 100  ) {
+    if (ypos > 20) {
+        element.style.animation = "showRectangle 300ms forwards";
         element.style.visibility = "visible";
         emtshape.style.animation = "rectangledash 5s alternate forwards";
+        // make sure to change above ^^ was 5s
         emtshape.style.animationDelay = "400ms";
 
 
     }
     else {
-        element.style.visibility = "hidden";
-        emtshape.style.animation =    }
+        element.style.visibility = "visible";
+        element.style.animation = "hideRectangle 300ms forwards";
+}
 }
 
+import skills from './skills.json' assert { type: 'json'};
+var cardnum = 0;
+
+function nextcard() {
+    if (cardnum >= 5) {
+        cardnum = 0;
+        document.getElementById("skillheader").innerHTML = skills.skills[cardnum].language;
+    }
+    else {
+        cardnum = cardnum + 1;
+        document.getElementById("skillheader").innerHTML = skills.skills[cardnum].language;
+    }
+
+}
+
+document.getElementById("rightbutton").addEventListener("click", nextcard)
 window.addEventListener("scroll", scrollAnim);
+
