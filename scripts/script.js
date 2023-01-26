8e4fde729710a1dca12734cb9f9dd31d2ff9970e
Bailey117 <116062175+Bailey117@users.noreply.github.com>
2023-01-26 15:34:50 +1100

    Ver 3.1
    Completed Skill Cards functionality, begun work on responsiveness relating to screen size.
    Adjusted opening page (index) background and text colours.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,12 @@
 var element = document.getElementById("skillcard")
 var emtshape = document.getElementById("skillcardshape")
+var skillheader = document.getElementById("skillheader")
+var skilltext = document.getElementById("skilltext")
+var leftside = document.getElementById("leftside")
+var rightside = document.getElementById("rightside")
+
+// animation: showside 1s forwards;
+// animation-delay: 4200ms;
 
 function scrollAnim() {
     var ypos = window.pageYOffset;
@@ -7,15 +14,27 @@ function scrollAnim() {
     if (ypos > 20) {
         element.style.animation = "showRectangle 300ms forwards";
         element.style.visibility = "visible";
-        emtshape.style.animation = "rectangledash 5s alternate forwards";
-        // make sure to change above ^^ was 5s
+        leftside.style.visibility = "visible";
+        rightside.style.visibility ="visible";
+        emtshape.style.animation = "rectangledash 4s alternate forwards";
         emtshape.style.animationDelay = "400ms";
+        skillheader.style.animation = "showRectangle 300ms forwards";
+        skillheader.style.animationDelay = "3500ms";
+        skilltext.style.animation = "showRectangle 300ms forwards";
+        skilltext.style.animationDelay = "3500ms";
+        leftside.style.animation = "showside 200ms forwards";
+        leftside.style.animationDelay = "3700ms"
+        rightside.style.animation = "showside 200ms forwards";
+        rightside.style.animationDelay = "3700ms"
+    
 
 
     }
     else {
         element.style.visibility = "visible";
         element.style.animation = "hideRectangle 300ms forwards";
+        leftside.style.visibility = "hidden";
+        rightside.style.visibility ="hidden";
 }
 }
 
@@ -27,22 +46,22 @@ function nextcard() {
         cardnum = 0;
         document.getElementById("skillheader").innerHTML = skills.skills[cardnum].language;
         document.getElementById("skilltext").innerHTML = skills.skills[cardnum].description;
+        document.getElementById("leftheader").innerHTML = skills.skills[5].language;
         document.getElementById("rightheader").innerHTML = skills.skills[(cardnum+1)].language;
-        document.getElementById("righttext").innerHTML = skills.skills[(cardnum+1)].description;
     }
     else if (cardnum == 4) {
         cardnum = cardnum + 1;
         document.getElementById("skillheader").innerHTML = skills.skills[cardnum].language;
         document.getElementById("skilltext").innerHTML = skills.skills[cardnum].description;
         document.getElementById("rightheader").innerHTML = skills.skills[0].language;
-        document.getElementById("righttext").innerHTML = skills.skills[0].description;
+        document.getElementById("leftheader").innerHTML = skills.skills[(cardnum-1)].language;
     }
     else {
         cardnum = cardnum + 1;
         document.getElementById("skillheader").innerHTML = skills.skills[cardnum].language;
         document.getElementById("skilltext").innerHTML = skills.skills[cardnum].description;
         document.getElementById("rightheader").innerHTML = skills.skills[(cardnum+1)].language;
-        document.getElementById("righttext").innerHTML = skills.skills[(cardnum+1)].description;
+        document.getElementById("leftheader").innerHTML = skills.skills[(cardnum-1)].language;
     }
 }
 
@@ -51,15 +70,36 @@ function prevcard() {
         cardnum = 5;
         document.getElementById("skillheader").innerHTML = skills.skills[cardnum].language;
         document.getElementById("skilltext").innerHTML = skills.skills[cardnum].description;
+        document.getElementById("leftheader").innerHTML = skills.skills[4].language;
+        document.getElementById("rightheader").innerHTML = skills.skills[(cardnum+1)].language;
+        console.log(cardnum)
+    }
+    else if (cardnum <= 1) {
+        cardnum = cardnum - 1;
+        document.getElementById("skillheader").innerHTML = skills.skills[cardnum].language;
+        document.getElementById("skilltext").innerHTML = skills.skills[cardnum].description;
+        document.getElementById("rightheader").innerHTML = skills.skills[0].language;
+        document.getElementById("leftheader").innerHTML = skills.skills[5].language;
+        console.log(cardnum)
+    }
+    else if (cardnum >= 5) {
+        cardnum = cardnum - 1;
+        document.getElementById("skillheader").innerHTML = skills.skills[4].language;
+        document.getElementById("skilltext").innerHTML = skills.skills[4].description;
+        document.getElementById("rightheader").innerHTML = skills.skills[5].language;
+        document.getElementById("leftheader").innerHTML = skills.skills[cardnum-1].language;
+        console.log(cardnum)
     }
     else {
         cardnum = cardnum - 1;
         document.getElementById("skillheader").innerHTML = skills.skills[cardnum].language;
         document.getElementById("skilltext").innerHTML = skills.skills[cardnum].description;
+        document.getElementById("rightheader").innerHTML = skills.skills[(cardnum+1)].language;
+        document.getElementById("leftheader").innerHTML = skills.skills[(cardnum-1)].language;
+        console.log(cardnum)
     }
 
 }
 document.getElementById("leftbutton").addEventListener("click", prevcard)
 document.getElementById("rightbutton").addEventListener("click", nextcard)
 window.addEventListener("scroll", scrollAnim);
-
